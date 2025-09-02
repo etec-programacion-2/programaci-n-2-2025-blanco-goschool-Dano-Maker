266d092800a18ce7ca6d721ccc1fb3121e3b3693
Negroooh <p.carreras@alumno.etec.um.edu.ar>
2025-09-02 12:55:16 -0300

    Issue número 2 Iniciada

--- app/src/main/kotlin/org/example/Materia.kt
+++ app/src/main/kotlin/org/example/Materia.kt
@@ -1,5 +1,12 @@
-package org.example
-
-class Materia { val id: Int, val nombre: String)
-        private val AlumnosInscritos=mutableListOf<Alumno>()
-    fun inscribirAlumno(alumn:Alumno)
+class Materia(val id: Int, val nombre: String) {
+    // Propiedad privada que almacena los alumnos inscriptos
+    private val alumnosInscriptos = mutableListOf<Alumno>()
+    // Método público para inscribir un alumno
+    fun inscribirAlumno(alumno: Alumno) {
+        alumnosInscriptos.add(alumno)
+    }
+    // Método público que devuelve una copia de solo lectura de la lista de alumnos inscriptos
+    fun obtenerAlumnosInscriptos(): List<Alumno> {
+        return alumnosInscriptos.toList() // Devuelve una copia inmutable
+    }
+}
