78fa746e83cc2495653fb64ee4669a60b035f932
Dano-Maker <mj.blanco@alumno.etec.um.edu.ar>
2025-10-17 10:36:08 -0300

    mitad de issue

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -11,16 +11,29 @@ fun main() {
     // Crear materias
     val idMatematicas = sistema.crearMateria("Matemáticas")
     val idHistoria = sistema.crearMateria("Historia")
+    val idFisica = sistema.crearMateria("Fisica")
+    val idTeleinformatica = sistema.crearMateria("Teleinformatica")
     
     // Registrar alumnos
     val idMati = sistema.registrarAlumno("Matias", "Blanco")
     val idCande = sistema.registrarAlumno("Candela", "Delacruz")
+    val idMaxi = sistema.registrarAlumno("Maximo", "Aznar")
+    val idIvo = sistema.registrarAlumno("Ivo", "Giovarruscio")
     
     // Inscribir alumnos en materias
     sistema.inscribirAlumnoEnMateria(idMati, idMatematicas)
-    sistema.inscribirAlumnoEnMateria(idCande, idMatematicas)
+    sistema.inscribirAlumnoEnMateria(idCande, idHistoria)
+    sistema.inscribirAlumnoEnMateria(idMaxi, idTeleinformatica)
+    sistema.inscribirAlumnoEnMateria(idIvo, idFisica)
     
     // Cargar notas
-    val eval = Evaluaciones("Trabajo Practico", 8.5)
-    sistema.cargarNota(idMati, idMatematicas, eval)
+    val eval_M = Evaluaciones("Trabajo Practico integrador", 8.5)
+    sistema.cargarNota(idMati, idMatematicas, eval_M)
+    val eval_H = Evaluaciones ("Examen escrito revolución francesa",9.0)
+    sistema.cargarNota(idCande, idHistoria, eval_H)
+    val eval_F = Evaluaciones("Examen MRUV", 9.5)
+    sistema.cargarNota(idIvo, idFisica, eval_F)
+    val eval_T = Evaluaciones("Evaluación redes WAN", 10.0)
+    sistema.cargarNota(idMaxi, idTeleinformatica, eval_T)
+
 }
